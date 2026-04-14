9040ea5c5e937bcd890f015fd0f9c04b9c90db15
nina333 <azkanina.busrie@gmail.com>
2026-04-14 17:08:58 +0800

    commit 5

--- nina.py
+++ nina.py
@@ -12,4 +12,10 @@ lihat()
 keranjang = ["Apel"]
 def tambah(item): keranjang.append(item)
 def hapus(): keranjang.pop() if keranjang else None
-hapus(); print(f"Setelah hapus: {keranjang}")
+hapus(); print(f"Setelah hapus: {keranjang}")keranjang = []
+while True:
+    item = input("Masukkan barang (ketik 'stop' untuk keluar): ")
+    if item == 'stop': break
+    keranjang.append(item)
+    print(f"Isi keranjang: {keranjang}")
+

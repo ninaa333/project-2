2dc6c206c1f04593d679fa3bd172867f86dc1eab
nina333 <azkanina.busrie@gmail.com>
2026-04-14 17:06:49 +0800

    commit 3

--- nina.py
+++ nina.py
@@ -3,3 +3,8 @@ print("Keranjang belanja siap!")
 
 def tambah(item): keranjang.append(item)
 tambah("Apel"); print(keranjang)
+
+keranjang = ["Apel"]
+def tambah(item): keranjang.append(item)
+def lihat(): print(f"Isi: {keranjang}")
+lihat()
